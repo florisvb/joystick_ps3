6a437646c1a7c3c44c8aaf4e7206e0e389ce3a88
Floris van Breugel <floris@sericomyia-mobile.(none)>
2010-08-19 22:26:19 -0700

    initializing values for buttons

--- nodes/ps3_interpreter.py
+++ nodes/ps3_interpreter.py
@@ -5,14 +5,38 @@ from joystick_ps3.msg import ps3values
 from joy.msg import Joy
 import time
 
+# need to be running: rosrun joy joy_node
+
 
 class Listener:
     def __init__(self):
-        self.msg = None
+        self.init_vals()
         rospy.Subscriber("joy", Joy, self.callback)
         self.pub = rospy.Publisher("ps3_interpreter", ps3values)
         rospy.init_node('ps3_interpreter', anonymous=True)
         self.run()
+        
+    def init_vals(self):
+    
+        self.msg = ps3values(   joyleft_x = 0,
+                                joyleft_y = 0,
+                                joyright_x = 0,
+                                joyright_y = 0,
+                                up = 0,
+                                down = 0,
+                                left = 0,
+                                right = 0,
+                                triangle = 0,
+                                x = 0,
+                                square = 0,
+                                circle = 0,
+                                select = 0,
+                                start = 0,
+                                playstation = 0,
+                                L1 = 0,
+                                L2 = 1,
+                                R1 = 0,
+                                R2 = 1)
 
     def callback(self, Joy):
         
